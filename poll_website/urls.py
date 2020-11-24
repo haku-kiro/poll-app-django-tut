b9129eb23745a0cef43654a7b4420ac85256d442
haku-kiro <botPrime@yahoo.com>
2020-11-24 11:52:44 +0200

    Adding part 1 files - with some notes

--- poll_website/urls.py
+++ poll_website/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # The include function allows us to call other url configs from apps,
+    # instead of having them all defined here
+    path('polls/', include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
