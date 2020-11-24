f1025a250fcbdab08634705d91fba401132f8253
haku-kiro <botPrime@yahoo.com>
2020-11-24 21:56:23 +0200

    Adding basic tests against models, currently going over how to implement tests against views - stopped at part 5: improving our view

--- polls/models.py
+++ polls/models.py
@@ -13,11 +13,12 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        '''
+        """
         Method to check if the question was published recently
         i.e. in the last day
-        '''
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
