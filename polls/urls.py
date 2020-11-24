22cacf16bea5cb4dd46944d5b6ed10037f99a17b
haku-kiro <botPrime@yahoo.com>
2020-11-24 19:56:37 +0200

    Adding generic views, completing some of the apps flow

--- polls/urls.py
+++ polls/urls.py
@@ -6,14 +6,29 @@ from . import views
 # Adding a namespace to our URLConfs file
 app_name = 'polls'
 
+# This was the old way, as in - without using generic views
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name="index"),
+#     # ex: /polls/5/
+#     # note, the question_id gets passed to the views function
+#     path('<int:question_id>/', views.detail, name="detail"),
+#     # ex: /poll/5/results/
+#     path('<int:question_id>/results/', views.results, name="results"),
+#     # ex: /poll/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name="vote"),
+#     # I just added the below as a test
+#     path('test/', views.test, name="test"),
+# ]
+
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name="index"),
+    path('', views.IndexView.as_view(), name="index"),
     # ex: /polls/5/
     # note, the question_id gets passed to the views function
-    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     # ex: /poll/5/results/
-    path('<int:question_id>/results/', views.results, name="results"),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name="results"),
     # ex: /poll/5/vote/
     path('<int:question_id>/vote/', views.vote, name="vote"),
     # I just added the below as a test
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+# from django.http import Http404
 # from django.template import loader
 
 # Making use of the models of the current app
-from .models import Question
+from .models import Question, Choice
+
+
+# Making use of generic views is better the manual views created below
+# refer to index, results, and detail
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions.
+        """
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 # Create your views here.
+# Note, this file contains index, detail, and results views - they are however
+# old and are just here as an example
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # output = ', '.join([q.question_text for q in latest_question_list])
@@ -30,12 +57,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    resp = "You're looking at the results of question %s."
-    return HttpResponse(resp % question_id)
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': q})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    q = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = q.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {'question': q,
+                                                     'error_message': "You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after
+        # Successfully dealing with post data
+        return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
 
 
 def test(request):
