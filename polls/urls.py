38ff6240e3206fb9866e5c6d79fbaa1a94deac2c
haku-kiro <botPrime@yahoo.com>
2020-11-24 15:02:32 +0200

    Adding part 3 files; Creating views and adding templates

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 
 from . import views
 
+
+# Adding a namespace to our URLConfs file
+app_name = 'polls'
+
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name="index"),
+    # ex: /polls/5/
+    # note, the question_id gets passed to the views function
+    path('<int:question_id>/', views.detail, name="detail"),
+    # ex: /poll/5/results/
+    path('<int:question_id>/results/', views.results, name="results"),
+    # ex: /poll/5/vote/
+    path('<int:question_id>/vote/', views.vote, name="vote"),
+    # I just added the below as a test
     path('test/', views.test, name="test"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,41 @@
-# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
+# from django.template import loader
+
+# Making use of the models of the current app
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world! You're at the polls index!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+
+    # template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+
+    # Returns a comma seperated list of the last 5 questions
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    resp = "You're looking at the results of question %s."
+    return HttpResponse(resp % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
 
 def test(request):
